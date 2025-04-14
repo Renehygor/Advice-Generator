65d41b53f2d956fedbb8df10621c479007974779
Renehygor <hygorrenebarbosa@gmail.com>
2025-04-14 09:21:36 -0300

    Retirei comentário desnecessários

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-// Passo 1 - pegar o botão 
-// Passo 2 - criar evento de click do botão
-// passo 3 - pegar elemento da descrição 
-// Passo 4 - fazer validação com try e catch cosa tenho algo errado
-// Passo 5 - consultar a url da API usando fetch e transformando em json
-// Passo 6 - mostrar o resultado trago pela API na descrição
 
-
-//resultado do desafio feito por mim 
-
-// botao.addEventListener("click",() =>{
-//     buscarConselho()
-// } );
-
-// async function buscarConselho() {
-//     const url = "https://api.adviceslip.com/advice";
-//     const resposta = await fetch(url); //fetch(url) → Faz uma requisição HTTP e retorna uma Promise.
-//     return await resposta.json();//.json() → Converte a resposta em JSON.
-// }
-
-// async function trazerConselho() {
-//     const resultado = await buscarConselho();
-//     const textoConselho = resultado.slip.advice.id;// caminho de busca do conselho
-//     descricao.textContent = textoConselho; // Aqui atualiza o conteúdo do elemento
-// }
-
-
-// Correção do curso
 const botao = document.querySelector("#botao");
 const numeroConselho = document.querySelector("#titulo")
 const conselho = document.querySelector("#descricao");
